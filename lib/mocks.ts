9006de1d9bc46084a6cdd922b1334188c51d0c6d
Patryk Górka <patryk.gorka@appunite.com>
2022-03-02 09:23:50 +0100

    feat: add mocks for cohort, modules and tasks

--- lib/mocks.ts
+++ lib/mocks.ts
@@ -1,3 +1,11 @@
+export interface Cohort {
+  id: string;
+  name: string;
+  inviteUrlId: string;
+  modules: Module[];
+  users: User[];
+}
+
 export interface User {
   id: string;
   name?: string;
@@ -6,16 +14,151 @@ export interface User {
   bio?: string;
   email?: string;
   image?: string;
+  cohortId: string;
 }
 
+export interface Module {
+  id: string;
+  name: string;
+  tasks: Task[];
+}
+
+type TaskType = 'code' | 'quiz' | 'image' | 'info';
+
+export interface Task {
+  id: string;
+  name: string;
+  moduleId: string;
+  type: TaskType;
+  description: string; //long text
+  data: null | string;
+  //data is a content of task which is different for each task type
+  //code -> repo link
+  //quiz -> quiz link
+  //image -> (probably) null
+  //info -> markdown text
+}
+
+type TaskStatus = 'todo' | 'done' | 'doing';
+export interface UserTask {
+  userId: string;
+  taskId: string;
+  status: TaskStatus;
+  attempt: number;
+  score: null | number; //grade
+  answer: null | string;
+  //answer is different for each task type
+  //code -> null - answers posted on github
+  //quiz -> null - automatic assessment after quiz submission using webhooks
+  //image -> image (probably image's url uploaded to some provider like cloudfront)
+  //info -> null - it's in todo or done
+}
+
+const mockedTasks: Task[] = [
+  {
+    id: 'task_id1',
+    name: 'Translate to a box diagram',
+    type: 'image',
+    data: null,
+    moduleId: 'mid',
+    description:
+      'Flutter is an app SDK for building high-performance, high-fidelity apps for iOS, Android, web and desktop from a single codebase.',
+  },
+  {
+    id: 'task_id2',
+    name: 'Flutter introduction',
+    type: 'info',
+    data: '### Markdown title',
+    moduleId: 'mid',
+    description: 'This is Flutter introduction info-type task!',
+  },
+  {
+    id: 'task_id3',
+    name: 'Flutter tour Quiz',
+    type: 'quiz',
+    data: 'https://link.to.quiz/quizhash',
+    moduleId: 'mid',
+    description: 'This is Flutter tour Quiz quiz-type task!',
+  },
+  {
+    id: 'task_id4',
+    name: 'Inheritance exercise',
+    type: 'code',
+    data: 'https://github.com/user/repo',
+    moduleId: 'mid',
+    description: 'This is Inheritance exercise code-type task!',
+  },
+];
+
+const mockedModules: Module[] = [
+  {
+    id: 'mid',
+    name: 'Basics',
+    tasks: mockedTasks,
+  },
+];
+
+//no way to access this before declaration
+
 const mockedUser: User = {
-  id: '35hk3hrkjh43kj5h',
+  id: 'userId',
   name: 'PatrykBuniX',
   firstname: 'Patryk',
   lastname: 'Górka',
   bio: 'Frontend developer in love with TypeScript and Next.js',
   email: 'patrykbunix@gmail.com',
   image: 'https://unsplash.it/100/100',
+  cohortId: 'cohortId',
 };
 
-export const db = { user: mockedUser };
+const mockedCohorts: Cohort[] = [
+  {
+    id: 'cohortId',
+    name: 'Toyota',
+    inviteUrlId: '123123',
+    modules: mockedModules,
+    users: [mockedUser],
+  },
+];
+
+const mockedUsersTasks: UserTask[] = [
+  {
+    userId: 'userId',
+    answer: null,
+    attempt: 0,
+    score: null,
+    status: 'doing',
+    taskId: 'task_id1',
+  },
+  {
+    userId: 'userId',
+    answer: null,
+    attempt: 0,
+    score: null,
+    status: 'todo',
+    taskId: 'task_id2',
+  },
+  {
+    userId: 'userId',
+    answer: null,
+    attempt: 0,
+    score: null,
+    status: 'todo',
+    taskId: 'task_id3',
+  },
+  {
+    userId: 'userId',
+    answer: null,
+    attempt: 0,
+    score: null,
+    status: 'todo',
+    taskId: 'task_id4',
+  },
+];
+
+export const db = {
+  user: mockedUser,
+  cohorts: mockedCohorts,
+  modules: mockedModules,
+  usersTasks: mockedUsersTasks,
+};
--- pages/tasks/index.tsx
+++ pages/tasks/index.tsx
@@ -1,10 +1,66 @@
 import { Layout } from '../../components/common/Layout/Layout';
 import { db } from '../../lib/mocks';
+import { GetServerSidePropsContext } from 'next';
+import { InferPagePropsType } from '../../lib/utils/types';
 
-export default function Tasks() {
+export default function Tasks({
+  user,
+  cohort,
+  module,
+  modules,
+  tasks,
+}: InferPagePropsType<typeof getServerSideProps>) {
+  console.log({ user, cohort, modules, module, tasks });
   return (
-    <Layout user={db.user}>
+    <Layout user={user}>
       <p>tasks content</p>
     </Layout>
   );
+}
+
+export async function getServerSideProps(ctx: GetServerSidePropsContext) {
+  //get user id from next auth, and fetch all his data using prisma.user.findFirst
+  const user = db.user;
+
+  //probably accessible when we declare that we want to include (populate) cohort relation
+  const cohorts = db.cohorts;
+  const userCohort = cohorts.find(c => c.id === user.cohortId);
+
+  //if user has no cohort, inform him that he has to ask a teacher for link
+  if (!userCohort) {
+    return {
+      props: { user, cohort: null, module: null, modules: [], tasks: [] },
+    };
+  }
+
+  //get selected module from url
+  const pickedModuleId = 'mid';
+
+  //cohort modules from prisma relation
+  const modules = userCohort.modules;
+
+  //use currently picked module
+  const module = modules.find(m => m.id === pickedModuleId);
+
+  //todo: if module is not picked, redirect to the one with 'in progress' task
+  if (!module) {
+    return {
+      props: { user, cohort: null, module: null, modules, tasks: [] },
+    };
+  }
+
+  //get tasks from selected module and their ids
+  const moduleTasks = module.tasks;
+  const moduleTasksIds = module.tasks.map(t => t.id);
+
+  //get user's tasks from UserTask table with matching task's ids from current module
+  const usersTasks = db.usersTasks;
+  const userTasks = usersTasks
+    .filter(ut => ut.userId === user.id && moduleTasksIds.includes(ut.taskId))
+    //we don't have to map it with prisma, it will be connected with relation
+    .map(ut => ({ ...ut, task: moduleTasks.find(t => t.id === ut.taskId) }));
+
+  return {
+    props: { user, cohort: userCohort, module, modules, tasks: userTasks },
+  };
 }
