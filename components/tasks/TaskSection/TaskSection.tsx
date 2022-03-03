951dfdf0b4aaa392aab55ba56bb31024b3f13e27
Patryk Górka <patryk.gorka@appunite.com>
2022-03-03 20:13:08 +0100

    feat: add description/information toggle buttons

--- components/tasks/TaskSection/TaskSection.tsx
+++ components/tasks/TaskSection/TaskSection.tsx
@@ -2,6 +2,8 @@ import styles from './TaskSection.module.scss';
 import { Module, Task, UserTask } from '../../../lib/mocks';
 import { TaskDescription } from '../TaskDescription/TaskDescription';
 import { TaskAction } from '../TaskAction/TaskAction';
+import { useState } from 'react';
+import clsx from 'clsx';
 
 interface TaskSectionProps {
   task: UserTask & { task: Task };
@@ -12,6 +14,7 @@ export const TaskSection = ({
   task: { task, status },
   module,
 }: TaskSectionProps) => {
+  const [isDescriptionView, setIsDescriptionView] = useState(true);
   return (
     <main className={styles.wrapper}>
       <div className={styles.taskHeader}>
@@ -24,7 +27,27 @@ export const TaskSection = ({
         <span className={styles.taskBadge}>{status}</span>
         <span className={styles.taskBadge}>{task.type}</span>
       </div>
-      <TaskDescription description={task.description} />
+      <div className={styles.taskNav}>
+        <button
+          onClick={() => setIsDescriptionView(true)}
+          className={clsx(
+            styles.taskNavButton,
+            isDescriptionView && styles.taskNavButtonActive
+          )}
+        >
+          Description
+        </button>
+        <button
+          onClick={() => setIsDescriptionView(false)}
+          className={clsx(
+            styles.taskNavButton,
+            !isDescriptionView && styles.taskNavButtonActive
+          )}
+        >
+          Information
+        </button>
+      </div>
+      {isDescriptionView && <TaskDescription description={task.description} />}
       <TaskAction task={task} />
     </main>
   );
