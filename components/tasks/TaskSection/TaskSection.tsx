2bfa3ae9b29ca5d674166143786e15a0d1862a15
Patryk Górka <patryk.gorka@appunite.com>
2022-03-04 22:38:46 +0100

    feat: add full screen mode for task section

--- components/tasks/TaskSection/TaskSection.tsx
+++ components/tasks/TaskSection/TaskSection.tsx
@@ -2,9 +2,11 @@ import styles from './TaskSection.module.scss';
 import { Module, Task, UserTask } from '../../../lib/mocks';
 import { TaskDescription } from '../TaskDescription/TaskDescription';
 import { TaskAction } from '../TaskAction/TaskAction';
-import { useState } from 'react';
+import { useCallback, useState } from 'react';
 import { TaskNav } from '../TaskNav/TaskNav';
 import EnlargeIcon from '../../../public/enlarge-icon.svg';
+import CrossIcon from '../../../public/cross-icon.svg';
+import clsx from 'clsx';
 
 interface TaskSectionProps {
   task: UserTask & { task: Task };
@@ -16,13 +18,28 @@ export const TaskSection = ({
   module,
 }: TaskSectionProps) => {
   const [isDescriptionView, setIsDescriptionView] = useState(true);
+  const [isFullScreenMode, setIsFullScreenMode] = useState(false);
+
+  const toggleFullScreenMode = useCallback(() => {
+    setIsFullScreenMode(prev => !prev);
+  }, []);
+
   return (
-    <main className={styles.wrapper}>
+    <main
+      className={clsx(
+        styles.wrapper,
+        isFullScreenMode && styles.wrapperFullScreen
+      )}
+    >
       <div className={styles.taskHeader}>
         <h2 className={styles.taskTitle}>{task.name}</h2>
         <span className={styles.taskModule}>{module.name}</span>
-        <button className={styles.fullScreenButton} aria-hidden>
-          <EnlargeIcon />
+        <button
+          onClick={toggleFullScreenMode}
+          className={styles.fullScreenButton}
+          aria-hidden
+        >
+          {isFullScreenMode ? <CrossIcon /> : <EnlargeIcon />}
         </button>
       </div>
       <div className={styles.taskBadges}>
