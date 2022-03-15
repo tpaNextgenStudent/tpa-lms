51c18251c2fd977d1fcd1bee4298b2705fd54b51
Patryk Górka <patryk.gorka@appunite.com>
2022-03-15 19:18:33 +0100

    fix: remove header button

--- pages/login/details.tsx
+++ pages/login/details.tsx
@@ -4,9 +4,7 @@ import { LoginHeroText } from '../../components/login/LoginHeroText/LoginHeroTex
 
 export default function LoginDetails() {
   return (
-    <LoginLayout
-      headerButton={{ text: 'login with github', onClick: () => {} }}
-    >
+    <LoginLayout>
       <LoginHeroText
         title="Well done"
         description="Last thing is to fill up 2 forms and type some of your hobbies"
