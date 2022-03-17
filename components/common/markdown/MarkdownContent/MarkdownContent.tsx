098cc727845ffe7cd43502d25c4a931d23549e84
Patryk Górka <patryk.gorka@appunite.com>
2022-03-17 20:17:49 +0100

    feat: add class for markdown-rendered elements styling

--- components/common/markdown/MarkdownContent/MarkdownContent.tsx
+++ components/common/markdown/MarkdownContent/MarkdownContent.tsx
@@ -1,6 +1,7 @@
 import ReactMarkdown from 'react-markdown';
 import rehypeRaw from 'rehype-raw';
 import { HandleCode } from '../HandleCode/HandleCode';
+import styles from './MarkdownContent.module.scss';
 
 interface MarkdownContentProps {
   content: string;
@@ -9,6 +10,7 @@ interface MarkdownContentProps {
 export const MarkdownContent = ({ content }: MarkdownContentProps) => {
   return (
     <ReactMarkdown
+      className={styles.markdown}
       rehypePlugins={[rehypeRaw]}
       components={{
         code({ inline, className, children, ...props }) {
