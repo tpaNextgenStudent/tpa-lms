5b666be45dedabcba231893808444ec29f8d7d49
Patryk Górka <patryk.gorka@appunite.com>
2022-03-18 21:47:28 +0100

    feat: add login layout max-width of 1440px

--- components/login/LoginLayout/LoginLayout.tsx
+++ components/login/LoginLayout/LoginLayout.tsx
@@ -18,38 +18,42 @@ export const LoginLayout = ({ children, fixedButton }: LoginLayoutProps) => {
           <CTAButton text={fixedButton.text} onClick={fixedButton.onClick} />
         </div>
       )}
-      <div className={styles.contentWrapper}>
-        <header className={styles.header}>
-          <div className={styles.logoWrapper}>
+      <div className={styles.contentBackgroundWrapper}>
+        <div className={styles.contentWrapper}>
+          <header className={styles.header}>
+            <div className={styles.logoWrapper}>
+              <Image
+                src="/tpa_logo.svg"
+                alt="Tech Play Academy Logo"
+                width={208}
+                height={20}
+              />
+            </div>
+          </header>
+          <main className={styles.main}>{children}</main>
+        </div>
+      </div>
+      <div className={styles.imageSectionBackgroundWrapper}>
+        <section
+          className={clsx(
+            styles.imageSection,
+            fixedButton && styles.imageSectionWithFixedButton
+          )}
+        >
+          <div className={styles.imageWrapper}>
             <Image
-              src="/tpa_logo.svg"
-              alt="Tech Play Academy Logo"
-              width={208}
-              height={20}
+              layout="responsive"
+              src={'/app_screen.png'}
+              width={547}
+              height={389}
+              className={styles.image}
             />
           </div>
-        </header>
-        <main className={styles.main}>{children}</main>
+          <div className={styles.svgWrapper} aria-hidden>
+            <BgStripesIcon />
+          </div>
+        </section>
       </div>
-      <section
-        className={clsx(
-          styles.imageSection,
-          fixedButton && styles.imageSectionWithFixedButton
-        )}
-      >
-        <div className={styles.imageWrapper}>
-          <Image
-            layout="responsive"
-            src={'/app_screen.png'}
-            width={547}
-            height={389}
-            className={styles.image}
-          />
-        </div>
-        <div className={styles.svgWrapper} aria-hidden>
-          <BgStripesIcon />
-        </div>
-      </section>
     </div>
   );
 };
