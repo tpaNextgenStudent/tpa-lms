a62219739c3bbc62bb082c0a091752a973697ac4
Patryk Górka <patryk.gorka@appunite.com>
2022-03-24 09:12:12 +0100

    refactor: move score and lock badges to separate components

--- components/tasks/TaskBadges/TaskBadges.tsx
+++ components/tasks/TaskBadges/TaskBadges.tsx
@@ -1,9 +1,9 @@
 import styles from './TaskBadges.module.scss';
-import clsx from 'clsx';
-import { Task, TaskStatus, TaskType, UserTask } from '../../../lib/utils/types';
-import LockIcon from '../../../public/lock-icon.svg';
+import { Task, UserTask } from '../../../lib/utils/types';
 import { TaskTypeBadge } from '../TaskTypeBadge/TaskTypeBadge';
 import { TaskStatusBadge } from '../TaskStatusBadge/TaskStatusBadge';
+import { TaskScoreBadge } from '../TaskScoreBadge/TaskScoreBadge';
+import { TaskLockBadge } from '../TaskLockBadge/TaskLockBadge';
 
 interface TaskBadgesProps {
   task: UserTask & { task: Task };
@@ -12,42 +12,10 @@ interface TaskBadgesProps {
 export const TaskBadges = ({ task }: TaskBadgesProps) => {
   return (
     <div className={styles.taskBadgesWrapper}>
-      {task.status === 'upcoming' && (
-        <span className={clsx(styles.taskLockWrapper)} aria-label="Task locked">
-          <LockIcon />
-        </span>
-      )}
-      {task.score && (
-        <span
-          className={clsx(
-            styles.taskScore,
-            styles[`taskScore${getColorByScore(task.score)}`]
-          )}
-        >
-          {task.score}
-        </span>
-      )}
+      {task.status === 'upcoming' && <TaskLockBadge />}
+      {task.score && <TaskScoreBadge score={task.score} />}
       <TaskStatusBadge status={task.status} />
       <TaskTypeBadge type={task.task.type} />
     </div>
   );
 };
-
-const SCORE_COLOR_MODIFIER = {
-  RED: 'Red',
-  ORANGE: 'Orange',
-  GREEN: 'Green',
-};
-
-function getColorByScore(score: number) {
-  switch (score) {
-    case 1:
-      return SCORE_COLOR_MODIFIER.RED;
-    case 2:
-      return SCORE_COLOR_MODIFIER.ORANGE;
-    case 3:
-      return SCORE_COLOR_MODIFIER.GREEN;
-    default:
-      return SCORE_COLOR_MODIFIER.GREEN;
-  }
-}
