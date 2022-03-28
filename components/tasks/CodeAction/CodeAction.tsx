e14a7113b8d0ee2735360038d9e827eced1bf09d
Patryk Górka <patryk.gorka@appunite.com>
2022-03-28 14:39:21 +0200

    refactor: move code action lines to separate component

--- components/tasks/CodeAction/CodeAction.tsx
+++ components/tasks/CodeAction/CodeAction.tsx
@@ -1,27 +1,17 @@
 import styles from './CodeAction.module.scss';
-import ClipboardIcon from '../../../public/clipboard-icon.svg';
-import TickIcon from '../../../public/tick-icon.svg';
 import { useEffect, useRef, useState, MouseEvent, useCallback } from 'react';
 import { Toast } from '../../common/Toast/Toast';
 import { useRouter } from 'next/router';
+import { CodeActionLines } from '../CodeActionLines/CodeActionLines';
 
 const sampleUrl =
   'https://github.com/tpa-nextgen/modules-master/blob/master/outlines/model_with_classes_1/task_formative_01.md';
 
-interface CodeLinesProps {
-  onMouseEnter?: (e: MouseEvent<HTMLDivElement>) => void;
-  lines: string[];
-}
-
-interface CodeLineProps {
-  value: string;
-  index: number;
-}
-
 export const CodeAction = () => {
+  const router = useRouter();
+
   const [isWarningVisible, setIsWarningVisible] = useState(false);
   const [isHoverDisabled, setIsHoverDisabled] = useState(false);
-  const router = useRouter();
 
   useEffect(() => {
     setIsHoverDisabled(false);
@@ -48,65 +38,18 @@ export const CodeAction = () => {
       <div className={styles.codeActionWrapper}>
         <div className={styles.codeActionBlock}>
           <p className={styles.codeText}>How to start</p>
-          <CodeLines
+          <CodeActionLines
             onMouseEnter={handleCloneHover}
             lines={[`git clone ${sampleUrl}`, 'git checkout -b my-branch']}
           />
         </div>
         <div className={styles.codeActionBlock}>
           <p className={styles.codeText}>Submit your work</p>
-          <CodeLines
+          <CodeActionLines
             lines={[`git commit -a -m solution`, 'git push origin my-branch']}
           />
         </div>
       </div>
     </div>
-  );
-};
-
-const CodeLines = ({ lines, onMouseEnter }: CodeLinesProps) => {
-  return (
-    <div className={styles.codeWrapper} onMouseEnter={onMouseEnter}>
-      <code className={styles.codeBlock}>
-        {lines.map((value, index) => {
-          return <CodeLine key={value} value={value} index={index} />;
-        })}
-      </code>
-    </div>
-  );
-};
-
-const CodeLine = ({ value, index }: CodeLineProps) => {
-  const [isCopied, setIsCopied] = useState(false);
-  const timeoutIdRef = useRef<ReturnType<typeof setTimeout>>();
-
-  const handleCopyClick = async (value: string) => {
-    await navigator.clipboard.writeText(value);
-    setIsCopied(true);
-    timeoutIdRef.current = setTimeout(() => {
-      setIsCopied(false);
-    }, 1000);
-  };
-
-  useEffect(() => {
-    return () => {
-      if (timeoutIdRef.current) {
-        clearTimeout(timeoutIdRef.current);
-      }
-    };
-  }, []);
-
-  return (
-    <pre className={styles.codeLine} key={value}>
-      <span>{value}</span>
-      <button
-        onClick={() => handleCopyClick(value)}
-        title={isCopied ? 'Copied' : 'Copy to clipboard'}
-        style={{ top: `calc(8px + ${index * 24}px)` }}
-        className={styles.clipboardButton}
-      >
-        {isCopied ? <TickIcon /> : <ClipboardIcon />}
-      </button>
-    </pre>
   );
 };
---
+++ components/tasks/CodeActionLines/CodeActionLines.tsx
@@ -0,0 +1,64 @@
+import styles from './CodeActionLines.module.scss';
+import { MouseEvent, useEffect, useRef, useState } from 'react';
+import TickIcon from '../../../public/tick-icon.svg';
+import ClipboardIcon from '../../../public/clipboard-icon.svg';
+
+interface CodeActionLinesProps {
+  onMouseEnter?: (e: MouseEvent<HTMLDivElement>) => void;
+  lines: string[];
+}
+
+interface CodeLineProps {
+  value: string;
+  index: number;
+}
+
+export const CodeActionLines = ({
+  lines,
+  onMouseEnter,
+}: CodeActionLinesProps) => {
+  return (
+    <div className={styles.codeWrapper} onMouseEnter={onMouseEnter}>
+      <code className={styles.codeBlock}>
+        {lines.map((value, index) => {
+          return <CodeLine key={value} value={value} index={index} />;
+        })}
+      </code>
+    </div>
+  );
+};
+
+const CodeLine = ({ value, index }: CodeLineProps) => {
+  const [isCopied, setIsCopied] = useState(false);
+  const timeoutIdRef = useRef<ReturnType<typeof setTimeout>>();
+
+  const handleCopyClick = async (value: string) => {
+    await navigator.clipboard.writeText(value);
+    setIsCopied(true);
+    timeoutIdRef.current = setTimeout(() => {
+      setIsCopied(false);
+    }, 1000);
+  };
+
+  useEffect(() => {
+    return () => {
+      if (timeoutIdRef.current) {
+        clearTimeout(timeoutIdRef.current);
+      }
+    };
+  }, []);
+
+  return (
+    <pre className={styles.codeLine} key={value}>
+      <span>{value}</span>
+      <button
+        onClick={() => handleCopyClick(value)}
+        title={isCopied ? 'Copied' : 'Copy to clipboard'}
+        style={{ top: `calc(8px + ${index * 24}px)` }}
+        className={styles.clipboardButton}
+      >
+        {isCopied ? <TickIcon /> : <ClipboardIcon />}
+      </button>
+    </pre>
+  );
+};
