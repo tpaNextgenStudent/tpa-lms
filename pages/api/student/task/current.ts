68dfe18524ce3cc517b5e1a5296771cfa9308db1
Paulina Pogorzelska <paulina.pogorzelska@appunite.com>
2022-04-05 12:48:28 +0200

    current task endpoint

--- pages/api/student/task/current.ts
+++ pages/api/student/task/current.ts
@@ -1,27 +1,54 @@
-// import { NextApiRequest, NextApiResponse } from 'next';
-// import prisma from '../../../../lib/prisma';
-// import getUserSession from '../../../../utils/getUserSession';
+import { NextApiRequest, NextApiResponse } from 'next';
+import prisma from '../../../../lib/prisma';
+import getUserSession from '../../../../utils/getUserSession';
 
-// export default async (req: NextApiRequest, res: NextApiResponse) => {
-//   const session = await getUserSession({ req });
-//   const userAssigment = await prisma.assignment.findFirst({
-//     where: { user_id: session.user?.id },
-//   });
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  const session = await getUserSession({ req });
+  const userAssigment = await prisma.assignment.findFirst({
+    where: { user_id: session.user?.id },
+  });
 
-//   const curriculum = await prisma.curriculum.findUnique({
-//     where: { assignment_id: userAssigment?.id },
-//   });
+  const curriculum = await prisma.curriculum.findUnique({
+    where: { assignment_id: userAssigment?.id },
+  });
 
-//   const module_progress = curriculum?.module_progress || [];
-//   const last_module_position = curriculum?.last_module_position;
-//   const last_task_position = curriculum?.last_task_position;
+  const module_progress = curriculum?.module_progress || [];
 
-//   const module =
-//     module_progress[last_module_position as keyof typeof module_progress];
+  const lastModulePositionIndex = (curriculum?.last_module_position || 1) - 1;
 
-//   if (session.nextAuthSession) {
-//     res.status(200).send({});
-//   } else {
-//     res.status(401).send({ message: 'Unauthorized' });
-//   }
-// };
+  const lastTaskPositionIndex = (curriculum?.last_task_position || 1) - 1;
+
+  const lastDoneTaskModule =
+    module_progress[lastModulePositionIndex as keyof typeof module_progress];
+
+  let response = {};
+  if (lastDoneTaskModule['tasks'][lastTaskPositionIndex + 1]) {
+    response = {
+      module_id:
+        module_progress[
+          lastModulePositionIndex as keyof typeof module_progress
+        ]['module_id'],
+      task_id:
+        module_progress[
+          lastModulePositionIndex as keyof typeof module_progress
+        ]['tasks'][lastTaskPositionIndex + 1]['id'],
+    };
+  } else {
+    response = {
+      module_id:
+        module_progress[
+          (lastModulePositionIndex + 1) as keyof typeof module_progress
+        ]['module_id'],
+      task_id:
+        module_progress[
+          (lastModulePositionIndex + 1) as keyof typeof module_progress
+        ]['tasks'][0]['id'],
+    };
+  }
+
+  if (session.nextAuthSession) {
+    res.status(200).send(response);
+  } else {
+    res.status(401).send({ message: 'Unauthorized' });
+  }
+};
