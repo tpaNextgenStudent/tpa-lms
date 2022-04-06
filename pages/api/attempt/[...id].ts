a5062457996a6c2c5bcac320db7986f46d97e7d4
Paulina Pogorzelska <paulina.pogorzelska@appunite.com>
2022-04-06 09:30:02 +0200

    modules counters

--- pages/api/attempt/[...id].ts
+++ pages/api/attempt/[...id].ts
@@ -1,4 +1,5 @@
 import { NextApiRequest, NextApiResponse } from 'next';
+import { responseSymbol } from 'next/dist/server/web/spec-compliant/fetch-event';
 import prisma from '../../../lib/prisma';
 import getUserSession from '../../../utils/getUserSession';
 
@@ -6,7 +7,15 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
   const session = await getUserSession({ req });
   const attempt_id = req.query.id[0];
 
-  const response = await prisma.attempt.findUnique({
+  const userAssigment = await prisma.assignment.findFirst({
+    where: { user_id: session.user?.id },
+  });
+
+  const curriculum = await prisma.curriculum.findUnique({
+    where: { assignment_id: userAssigment?.id },
+  });
+
+  const attempt = await prisma.attempt.findUnique({
     where: { id: attempt_id },
     select: {
       id: true,
@@ -18,9 +27,19 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
       attempt_number: true,
       submission_date: true,
       evaluation_date: true,
+      status: true,
       teacher: { select: { user: true } },
     },
   });
+
+  const moduleId = attempt?.task.module_version_id;
+  const curriculumProgress = (curriculum?.module_progress as Array<any>) || [];
+
+  const moduleNumber = curriculumProgress.find(
+    module => module.module_id === moduleId
+  ).position;
+
+  const response = { ...attempt, module_numer: moduleNumber };
 
   if (session.nextAuthSession) {
     res.status(200).send(response);
