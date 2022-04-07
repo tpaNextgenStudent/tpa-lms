de920bc795007994cd81a7dc13b48a326ade0b1f
Patryk Górka <patryk.gorka@appunite.com>
2022-04-07 22:09:29 +0200

    style: fix module select styles

--- components/common/ModuleSelect/ModuleSelect.tsx
+++ components/common/ModuleSelect/ModuleSelect.tsx
@@ -1,5 +1,6 @@
 import styles from './ModuleSelect.module.scss';
 import colors from '../../../lib/styles/variables/colors.module.scss';
+import fonts from '../../../lib/styles/variables/fonts.module.scss';
 import ArrowDownIcon from '../../../public/arrow-down.svg';
 
 import Select, {
@@ -23,16 +24,24 @@ const customStyles: StylesConfig<OptionType, IsMulti> = {
     borderRadius: state.menuIsOpen ? '0' : '64px',
     borderTopLeftRadius: state.menuIsOpen ? '24px' : '64px',
     borderTopRightRadius: state.menuIsOpen ? '24px' : '64px',
-    border: `1px solid ${colors.strokeMain}`,
-    borderBottom: state.menuIsOpen ? '0' : `1px solid ${colors.strokeMain}`,
     flexWrap: 'nowrap',
     height: '48px',
+    boxShadow: 'none',
+    transition: 'none',
+    border: state.isFocused
+      ? `1px solid ${colors.purplePrimary}`
+      : `1px solid ${colors.strokeMain}`,
+    borderBottom: state.menuIsOpen
+      ? '0'
+      : `1px solid ${
+          state.isFocused ? colors.purplePrimary : colors.strokeMain
+        }`,
     margin: 0,
     minWidth: '108px',
     width: '100%',
     ':hover': {
-      border: `1px solid ${colors.strokeMain}`,
-      borderBottom: state.menuIsOpen ? '0' : `1px solid ${colors.strokeMain}`,
+      border: `1px solid ${colors.purplePrimary}`,
+      borderBottom: !state.menuIsOpen ? `1px solid ${colors.purplePrimary}` : 0,
       backgroundColor: colors.bgTable,
     },
   }),
@@ -46,7 +55,7 @@ const customStyles: StylesConfig<OptionType, IsMulti> = {
   }),
   singleValue: (provided, state) => ({
     ...provided,
-    fontWeight: 400,
+    fontWeight: fonts.weightRegular,
     padding: '8px 0 8px 16px',
     fontSize: '12px',
     lineHeight: '24px',
@@ -73,7 +82,20 @@ const customStyles: StylesConfig<OptionType, IsMulti> = {
     borderBottomLeftRadius: '24px',
     borderBottomRightRadius: '24px',
     boxShadow: 'none',
-    border: `1px solid ${colors.strokeMain}`,
+    border: `1px solid ${colors.purplePrimary}`,
+    borderTop: 0,
+    position: 'relative',
+    '::before': {
+      content: "''",
+      position: 'absolute',
+      width: 'calc(100% - 32px)',
+      height: '1px',
+      backgroundColor: colors.strokeMain,
+      top: 0,
+      left: '50%',
+      transform: 'translateX(-50%)',
+      zIndex: 1,
+    },
   }),
   menuList: (provided, state) => ({
     ...provided,
@@ -84,7 +106,7 @@ const customStyles: StylesConfig<OptionType, IsMulti> = {
   option: (provided, state) => ({
     ...provided,
     padding: '12px 16px',
-    fontWeight: 500,
+    fontWeight: fonts.weightMedium,
     fontSize: '12px',
     lineHeight: '24px',
     color: colors.fontPrimary,
