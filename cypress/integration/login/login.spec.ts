a62c53425bb70fa5fd23f3b7c07889fd21ffb776
Patryk Górka <patryk.gorka@appunite.com>
2022-04-09 15:35:04 +0200

    test: test login with github

--- cypress/integration/login/login.spec.ts
+++ cypress/integration/login/login.spec.ts
@@ -1,9 +1,40 @@
-describe('Login page', () => {
-  beforeEach(() => {
-    cy.visit('/login');
-  });
+import Cookie = Cypress.Cookie;
 
-  it('displays sign-in button', () => {
-    cy.get('button').contains('Sign in').should('exist');
+describe('Login', () => {
+  it('Success for GitHub provider', () => {
+    const cookiesName = Cypress.env('COOKIE_NAME');
+    const socialLoginOptions = {
+      username: Cypress.env('GITHUB_USER'),
+      password: Cypress.env('GITHUB_PW'),
+      loginUrl: Cypress.config().baseUrl + '/login',
+      loginSelector: 'button',
+      postLoginSelector: 'ul',
+      getAllBrowserCookies: true,
+    };
+
+    return cy
+      .task('GitHubSocialLogin', socialLoginOptions)
+      .then(({ cookies }: any) => {
+        cy.clearCookies();
+
+        const filteredCookies = cookies.filter((cookie: Cookie) =>
+          cookiesName.includes(cookie.name)
+        );
+        filteredCookies.forEach((cookie: Cookie) => {
+          cy.setCookie(cookie.name, cookie.value, {
+            domain: cookie.domain,
+            expiry: cookie.expiry,
+            httpOnly: cookie.httpOnly,
+            path: cookie.path,
+            secure: cookie.secure,
+          });
+        });
+
+        Cypress.Cookies.defaults({
+          preserve: cookiesName,
+        });
+
+        cy.visit('');
+      });
   });
 });
