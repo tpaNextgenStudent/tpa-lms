bc4cc38057a18355e95b3756003cbc77e5e71e9f
Patryk Górka <patryk.gorka@appunite.com>
2022-04-11 08:28:19 +0200

    style: update login page styles

--- components/common/UserNav/UserNav.tsx
+++ components/common/UserNav/UserNav.tsx
@@ -6,6 +6,7 @@ import { useRef, useState } from 'react';
 import clsx from 'clsx';
 import { useClickOutside } from '../../../lib/useClickOutside';
 import { IUserDetails } from '../../../api/user';
+import { signOut } from 'next-auth/react';
 
 interface UserNavProps {
   user: IUserDetails;
@@ -86,7 +87,12 @@ export const UserNav = ({ user }: UserNavProps) => {
             </Link>
           </li>
           <li className={styles.userNavListItem}>
-            <button className={styles.userNavLogoutButton}>Log out</button>
+            <button
+              onClick={() => signOut()}
+              className={styles.userNavLogoutButton}
+            >
+              Log out
+            </button>
           </li>
         </ul>
       </div>
