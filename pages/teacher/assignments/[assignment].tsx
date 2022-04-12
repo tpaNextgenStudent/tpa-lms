cdee53b4e711672bf2bc9ba3e36568506efe5a8a
Patryk Górka <patryk.gorka@appunite.com>
2022-04-12 22:04:40 +0200

    fix: change login error copy and add back to login button

---
+++ pages/teacher/assignments/[assignment].tsx
@@ -0,0 +1,38 @@
+import { Layout } from '../../../components/common/Layout/Layout';
+import { InferPagePropsType } from '../../../lib/utils/types';
+import { withServerSideAuth } from '../../../lib/auth/withServerSideAuth';
+import { getUserDetails } from '../../../api/user';
+import { getAttemptById } from '../../../api/attempts';
+
+export default function ScoresIndex({
+  user,
+}: InferPagePropsType<typeof getServerSideProps>) {
+  return (
+    <Layout
+      title="Assignments"
+      // title={task.name}
+      user={user}
+      headerPrevButton={{ pageName: 'My Scores', pageLink: '/student/scores' }}
+    >
+      <p>single assignment to check</p>
+    </Layout>
+  );
+}
+export const getServerSideProps = withServerSideAuth(
+  async ({ req, params }) => {
+    const { assignment: assignmentId } = params! as {
+      assignment: string;
+    };
+
+    const authCookie = req.headers.cookie as string;
+    const user = await getUserDetails({ cookie: authCookie });
+
+    const attempt = await getAttemptById(assignmentId, { cookie: authCookie });
+
+    return {
+      props: {
+        user,
+      },
+    };
+  }
+);
