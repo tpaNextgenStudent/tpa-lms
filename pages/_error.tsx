ee31822ed8fa4837d81a01833eda0fc7712dc053
Patryk Górka <patryk.gorka@appunite.com>
2022-04-13 20:58:21 +0200

    fix: update error page copy

--- pages/_error.tsx
+++ pages/_error.tsx
@@ -1,20 +1,32 @@
 import { NextPageContext } from 'next';
 import { ErrorView } from '../components/common/ErrorView/ErrorView';
 import { useRouter } from 'next/router';
+import { signOut } from 'next-auth/react';
 
 export default function Error({
   statusCode,
 }: ReturnType<typeof Error.getInitialProps>) {
   const router = useRouter();
-  const { title, description } = statusToMessage(statusCode);
+  const { title, description, withHomeRedirect } = statusToMessage(statusCode);
+
+  const buttonSettings = withHomeRedirect
+    ? {
+        text: 'Go back to home page',
+        onClick: () => router.push('/'),
+      }
+    : {
+        text: 'Back to login page',
+        onClick: async () => {
+          await signOut();
+          await router.push('/');
+        },
+      };
+
   return (
     <ErrorView
       title={title}
       description={description}
-      primaryButton={{
-        text: 'Go back to home page',
-        onClick: () => router.push('/'),
-      }}
+      primaryButton={buttonSettings}
     />
   );
 }
@@ -27,21 +39,22 @@ Error.getInitialProps = ({ res, err }: NextPageContext) => {
 function statusToMessage(status: number | undefined): {
   title: string;
   description: string;
+  withHomeRedirect?: boolean;
 } {
-  if (!status) return { title: `*Error*`, description: 'An error occurred!' };
+  if (!status)
+    return {
+      title: `*Something went wrong*`,
+      description: 'Go back to the login page and try again.',
+    };
   if (status === 404)
     return {
       title: '*404 Not found*',
       description: 'This page does not exist!',
-    };
-  if (status >= 500)
-    return {
-      title: `*${status} Error*`,
-      description: 'Internal Server Error :(',
+      withHomeRedirect: true,
     };
 
   return {
-    title: `*${status} Error*`,
-    description: 'Client-side error occurred!',
+    title: `*Something went wrong (code: ${status})*`,
+    description: 'Go back to the login page and try again.',
   };
 }
