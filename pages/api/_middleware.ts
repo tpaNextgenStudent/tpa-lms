e977884d6b65fe3728e0f949e71607ba154665e1
Paulina Pogorzelska <paulina.pogorzelska@appunite.com>
2022-04-19 12:14:05 +0200

    fix

--- pages/api/_middleware.ts
+++ pages/api/_middleware.ts
@@ -1,12 +1,12 @@
-import { getSession } from 'next-auth/react';
+// import { getSession } from 'next-auth/react';
 import { NextApiRequest, NextApiResponse } from 'next';
 
 const middleware = async (req: NextApiRequest, ev: NextApiResponse) => {
   //Require session in all endpoints except login
 
   if (!req.url?.includes('api/auth')) {
-    const session = await getSession({ req });
-    console.log(session);
+    // const session = await getSession({ req });
+    // console.log(session);
     // if (!session) {
     //   const obj = { message: 'unauthorized' };
     //   const blob = new Blob([JSON.stringify(obj, null, 2)], {
