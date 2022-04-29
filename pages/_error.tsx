077392f83df9b8534c302d15ac97ad37924de61d
Paulina Pogorzelska <paulina.pogorzelska@appunite.com>
2022-04-29 11:04:13 +0200

    v3

--- pages/_error.tsx
+++ pages/_error.tsx
@@ -12,7 +12,7 @@ export default function Error() {
       primaryButton={{
         text: 'Back to login page',
         onClick: async () => {
-          await axios.post('http://localhost:3000/api/upload/curriculumBase', {
+          await axios.post('http://localhost:3000/api/createCurriculum/', {
             body: {
               name: 'toyota-02',
               modules: [
@@ -25,7 +25,7 @@ export default function Error() {
                       name: 'Class with properties that capture the data aspects of a core noun',
                       type: 'info',
                       description:
-                        'https://raw.githubusercontent.com/tpa-nextgen/modules-master/master/outlines/model_with_classes_1/mwc1.info.properties.md?token=GHSAT0AAAAAABTYCY45NZCOKFW32R4XZH26YTKUPLA',
+                        'https://github.com/tpa-nextgen/modules-master/blob/master/outlines/model_with_classes_1/mwc1.info.properties.md',
                       link: null,
                       kind: 'formative',
                     },
@@ -34,7 +34,7 @@ export default function Error() {
                       name: 'Class with constructor that initialize the data',
                       type: 'info',
                       description:
-                        'https://raw.githubusercontent.com/tpa-nextgen/modules-master/master/outlines/model_with_classes_1/mvw1.info.constructor.md?token=GHSAT0AAAAAABTYCY45PUM3W4MWWX5IXYRUYTKUPSQ',
+                        'https://github.com/tpa-nextgen/modules-master/blob/master/outlines/model_with_classes_1/mvw1.info.constructor.md',
                       link: null,
                       kind: 'formative',
                     },
@@ -43,7 +43,7 @@ export default function Error() {
                       name: 'Class methods which change class state',
                       type: 'info',
                       description:
-                        'https://raw.githubusercontent.com/tpa-nextgen/modules-master/master/outlines/model_with_classes_1/mwc1.info.class_methods.md?token=GHSAT0AAAAAABTYCY444O3YM57HSZCSQZWQYTKUQAQ',
+                        'https://github.com/tpa-nextgen/modules-master/blob/master/outlines/model_with_classes_1/mwc1.info.class_methods.md',
                       link: null,
                       kind: 'formative',
                     },
@@ -52,7 +52,7 @@ export default function Error() {
                       name: 'Toggl task',
                       type: 'code',
                       description:
-                        'https://raw.githubusercontent.com/tpa-nextgen/modules-master/master/outlines/model_with_classes_1/mwc1.code.dart.toggl_task.md?token=GHSAT0AAAAAABTYCY443LZJRU53ZZTFDGOYYTKUQJQ',
+                        'https://github.com/tpa-nextgen/modules-master/blob/master/outlines/model_with_classes_1/mwc1.code.dart.toggl_task.md',
                       link: 'https://github.com/tpa-nextgen/mwc1.tf16.toggl_task.code.dart',
                       kind: 'formative',
                     },
@@ -61,7 +61,7 @@ export default function Error() {
                       name: 'Spotify playlist',
                       type: 'code',
                       description:
-                        'https://raw.githubusercontent.com/tpa-nextgen/modules-master/master/outlines/model_with_classes_1/mwc1.code.dart.spotify_playlist.md?token=GHSAT0AAAAAABTYCY456MI5VDQW3MRXIKAOYTKUQRQ',
+                        'https://github.com/tpa-nextgen/modules-master/blob/master/outlines/model_with_classes_1/mwc1.code.dart.spotify_playlist.md',
                       link: 'https://github.com/tpa-nextgen/mwc1.spotify_playlist.code.dart',
                       kind: 'summative',
                     },
@@ -76,7 +76,7 @@ export default function Error() {
                       name: 'Objects as a Property',
                       type: 'info',
                       description:
-                        'https://raw.githubusercontent.com/tpa-nextgen/modules-master/1cd29d1f9fc592be29aa523d7500cf60aa6b0725/outlines/model_with_classes_1/mwc1.info.dart.aggregation.md?token=GHSAT0AAAAAABTYCY44FYPHU6JPGDHGM5SQYTKUSLQ',
+                        'https://github.com/tpa-nextgen/modules-master/blob/1cd29d1f9fc592be29aa523d7500cf60aa6b0725/outlines/model_with_classes_1/mwc1.info.dart.aggregation.md',
                       link: null,
                       kind: 'formative',
                     },
@@ -85,7 +85,7 @@ export default function Error() {
                       name: 'Movies database',
                       type: 'code',
                       description:
-                        'https://raw.githubusercontent.com/tpa-nextgen/modules-master/1cd29d1f9fc592be29aa523d7500cf60aa6b0725/outlines/model_with_classes_1/mwc1.code.dart.movies_database.md?token=GHSAT0AAAAAABTYCY45TH7MJDXM6FHBNEFMYTKUSUA',
+                        'https://github.com/tpa-nextgen/modules-master/blob/1cd29d1f9fc592be29aa523d7500cf60aa6b0725/outlines/model_with_classes_1/mwc1.code.dart.movies_database.md',
                       link: 'https://github.com/tpa-nextgen/mwc1.tf07.movies_database.code.dart',
                       kind: 'formative',
                     },
@@ -94,7 +94,7 @@ export default function Error() {
                       name: 'Creating a Copy of an Object',
                       type: 'info',
                       description:
-                        'https://raw.githubusercontent.com/tpa-nextgen/modules-master/1cd29d1f9fc592be29aa523d7500cf60aa6b0725/outlines/model_with_classes_1/mwc1.info.dart.copy_of_object.md?token=GHSAT0AAAAAABTYCY447BIHWHBIVH7IKSHQYTKUS6Q',
+                        'https://github.com/tpa-nextgen/modules-master/blob/1cd29d1f9fc592be29aa523d7500cf60aa6b0725/outlines/model_with_classes_1/mwc1.info.dart.copy_of_object.md',
                       link: null,
                       kind: 'formative',
                     },
@@ -103,7 +103,7 @@ export default function Error() {
                       name: 'Toggl task',
                       type: 'code',
                       description:
-                        'https://raw.githubusercontent.com/tpa-nextgen/modules-master/1cd29d1f9fc592be29aa523d7500cf60aa6b0725/outlines/model_with_classes_1/mwc1.code.dart.movies_statistics.md?token=GHSAT0AAAAAABTYCY45LF3EYKA5FRXSSS6UYTKUTFA',
+                        'https://github.com/tpa-nextgen/modules-master/blob/1cd29d1f9fc592be29aa523d7500cf60aa6b0725/outlines/model_with_classes_1/mwc1.code.dart.movies_statistics.md',
                       link: 'https://github.com/tpa-nextgen/mwc1.tf10.movies_statistics.code.dart',
                       kind: 'summative',
                     },
@@ -118,7 +118,7 @@ export default function Error() {
                       name: 'Access modifiers',
                       type: 'info',
                       description:
-                        'https://raw.githubusercontent.com/tpa-nextgen/modules-master/1cd29d1f9fc592be29aa523d7500cf60aa6b0725/outlines/model_with_classes_1/mwc1.info.dart.access_modifiers.md?token=GHSAT0AAAAAABTYCY45WIBOIMBYUK2XTQAEYTKUTUA',
+                        'https://github.com/tpa-nextgen/modules-master/blob/1cd29d1f9fc592be29aa523d7500cf60aa6b0725/outlines/model_with_classes_1/mwc1.info.dart.access_modifiers.md',
                       link: null,
                       kind: 'formative',
                     },
@@ -127,7 +127,7 @@ export default function Error() {
                       name: 'Immutability',
                       type: 'info',
                       description:
-                        'https://raw.githubusercontent.com/tpa-nextgen/modules-master/1cd29d1f9fc592be29aa523d7500cf60aa6b0725/outlines/model_with_classes_1/mwc1.info.dart.immutability.md?token=GHSAT0AAAAAABTYCY45BUSBMLXHOAH7K7ROYTKUT3A',
+                        'https://github.com/tpa-nextgen/modules-master/blob/1cd29d1f9fc592be29aa523d7500cf60aa6b0725/outlines/model_with_classes_1/mwc1.info.dart.immutability.md',
                       link: null,
                       kind: 'formative',
                     },
@@ -136,7 +136,7 @@ export default function Error() {
                       name: 'Static Properties and Methods',
                       type: 'info',
                       description:
-                        'https://raw.githubusercontent.com/tpa-nextgen/modules-master/1cd29d1f9fc592be29aa523d7500cf60aa6b0725/outlines/model_with_classes_1/mwc1.info.dart.static.md?token=GHSAT0AAAAAABTYCY45DJIGOOUQE4KDXV6GYTKUUCQ',
+                        'https://github.com/tpa-nextgen/modules-master/blob/1cd29d1f9fc592be29aa523d7500cf60aa6b0725/outlines/model_with_classes_1/mwc1.info.dart.static.md',
                       link: null,
                       kind: 'formative',
                     },
@@ -145,7 +145,7 @@ export default function Error() {
                       name: 'Scope',
                       type: 'info',
                       description:
-                        'https://raw.githubusercontent.com/tpa-nextgen/modules-master/1cd29d1f9fc592be29aa523d7500cf60aa6b0725/outlines/model_with_classes_1/mwc1.info.dart.scope.md?token=GHSAT0AAAAAABTYCY44D7VDSXPW4K2EKSPCYTKUUQQ',
+                        'https://github.com/tpa-nextgen/modules-master/blob/1cd29d1f9fc592be29aa523d7500cf60aa6b0725/outlines/model_with_classes_1/mwc1.info.dart.scope.md',
                       link: null,
                       kind: 'formative',
                     },
@@ -154,7 +154,7 @@ export default function Error() {
                       name: 'Task manager',
                       type: 'code',
                       description:
-                        'https://raw.githubusercontent.com/tpa-nextgen/modules-master/1cd29d1f9fc592be29aa523d7500cf60aa6b0725/outlines/model_with_classes_1/mwc1.code.dart.tasks_manager.md?token=GHSAT0AAAAAABTYCY44UMNFAXIXTZYL7PWCYTKUU3A',
+                        'https://github.com/tpa-nextgen/modules-master/blob/1cd29d1f9fc592be29aa523d7500cf60aa6b0725/outlines/model_with_classes_1/mwc1.code.dart.tasks_manager.md',
                       link: 'https://github.com/tpa-nextgen/mwc1.tf16.tasks_manager.code.dart',
                       kind: 'summative',
                     },
