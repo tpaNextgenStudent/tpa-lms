b05667fee25ee7688d608b294d71690f7cb13eea
Patryk Górka <patryk.gorka@appunite.com>
2022-05-23 10:29:47 +0200

    tests: add CTAButton component story

---
+++ stories/CTAButton.stories.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import { CTAButton } from '../components/common/CTAButton/CTAButton';
+
+export default {
+  title: 'Buttons/CTAButton',
+  component: CTAButton,
+} as ComponentMeta<typeof CTAButton>;
+
+const Template: ComponentStory<typeof CTAButton> = args => (
+  <CTAButton {...args} />
+);
+
+export const Primary = Template.bind({});
+Primary.args = {
+  text: 'Mark as read',
+  styleType: 'primary',
+  isDisabled: false,
+  type: 'button',
+};
+
+export const Secondary = Template.bind({});
+Secondary.args = {
+  text: 'Mark as read',
+  styleType: 'secondary',
+  isDisabled: false,
+  type: 'button',
+};
