23e5d7a13db2d1f0ea51e0b35be7f6c6fb73fbac
Patryk Górka <patryk.gorka@appunite.com>
2022-05-24 12:18:27 +0200

    tests: add table UserNameCell component story

---
+++ components/common/tables/UserNameCell/UserNameCell.stories.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import { UserNameCell } from './UserNameCell';
+
+export default {
+  title: 'Tables/UserNameCell',
+  component: UserNameCell,
+  parameters: { controls: { exclude: ['img'] } },
+} as ComponentMeta<typeof UserNameCell>;
+
+const Template: ComponentStory<typeof UserNameCell> = args => (
+  <UserNameCell {...args} />
+);
+
+export const Default = Template.bind({});
+Default.args = {
+  name: 'Patryk Górka',
+  login: 'patrykbunix',
+  id: 'userId',
+};
+
+export const WithoutId = Template.bind({});
+WithoutId.args = {
+  name: 'Patryk Górka',
+  login: 'patrykbunix',
+};
