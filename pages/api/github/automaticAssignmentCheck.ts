6ea8f644b5850d07ac837abba035ad8838008e49
Paulina Pogorzelska <paulina.pogorzelska@appunite.com>
2022-05-24 23:47:29 +0200

    github merge 1

--- pages/api/github/automaticAssignmentCheck.ts
+++ pages/api/github/automaticAssignmentCheck.ts
@@ -1,19 +1,26 @@
 import { NextApiRequest, NextApiResponse } from 'next';
-import { Octokit, App } from 'octokit';
+import { Octokit } from 'octokit';
 import prisma from '../../../lib/prisma';
-import curriculumBase from '../upload/curriculumBase';
 
-// Info from Workflow runs webhook
-// workflow_run.head_branch must match user personal branch name
-// we're finding artefat_id by run_id (id)
 const findTaskInfo = (repositoryUrl: string) => {
   const repositoryUrlSplitted = repositoryUrl.split('-');
   const userLogin = repositoryUrlSplitted.pop();
   return userLogin;
 };
 
-const findTaskDetails = async (repositoryUrl: string) => {
-  const userLogin = findTaskInfo(repositoryUrl);
+const findTaskDetails = async (
+  repositoryUrl: string,
+  userLogin: string,
+  res: NextApiResponse
+) => {
+  const userRepoLogin = findTaskInfo(repositoryUrl);
+
+  if (userRepoLogin != userLogin) {
+    res.status(404).send({
+      message:
+        'User that made action is not a user that should make actions on this repository',
+    });
+  }
 
   const userProfile = await prisma.profile.findUnique({
     where: { login: userLogin },
@@ -34,13 +41,7 @@ const findTaskDetails = async (repositoryUrl: string) => {
 };
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-  // const payload = JSON.parse(req.body.payload);
-  const payload = {
-    repository: {
-      html_url:
-        'https://github.com/tpa-nextgen-staging/mwc1.tf16.tasks_manager.code.dart-PaulinaPogorzelska',
-    },
-  };
+  const payload = JSON.parse(req.body.payload);
 
   const octokit = new Octokit({
     auth: process.env.GITHUB_PERSONAL_ACCESS_TOKEN,
@@ -50,7 +51,11 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
     data: { login },
   } = await octokit.rest.users.getAuthenticated();
 
-  const taskDetails = await findTaskDetails(payload.repository.html_url);
+  const taskDetails = await findTaskDetails(
+    payload.repository.html_url,
+    payload.workflow_run.actor.login,
+    res
+  );
 
   // //Check if it was pull_request event
   // if (payload.workflow_run.event != 'pull_request') {
@@ -62,21 +67,7 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
   //   res.status(404).send({ message: 'Incorrect branch name' });
   // }
 
-  //Find if user that performed request is our student
-  //payload.workflow_run.actor.login
-  // TODO
-  // const studentCurriculum = await prisma.profile.findUnique({
-  //   where: {
-  //     login: payload.workflow_run.actor.login,
-  //   },
-  //   select: { assignments: { select: { curriculum: true } } },
-  // });
-
-  //If user is our student find their curricumum
-
-  //In their progress find task with this link
-
-  //Fetch task details and check if task is summative or formative
+  // //Check if task is summative or formative
 
   // if (payload.action === 'requested') {
   //   //Mark attempt as in review if action is requested
@@ -87,61 +78,61 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
   //   //Make all actions for completed state
   //   const runId = payload.workflow_run.id;
 
-  // const runJobs = (await octokit
-  //   .request('GET /repos/{owner}/{repo}/actions/runs/{run_id}/jobs', {
-  //     repo: 'mwc1.tf16.toggl_task.code.dart',
-  //     owner: 'tpa-nextgen',
-  //     run_id: 2351531697,
-  //   })
-  //   .catch(e => console.log(e))) as any;
+  //   const runJobs = (await octokit
+  //     .request('GET /repos/{owner}/{repo}/actions/runs/{run_id}/jobs', {
+  //       repo: 'mwc1.tf16.toggl_task.code.dart',
+  //       owner: 'tpa-nextgen',
+  //       run_id: 2351531697,
+  //     })
+  //     .catch(e => console.log(e))) as any;
 
-  // const logs = (await octokit
-  //   .request('GET /repos/{owner}/{repo}/actions/jobs/{job_id}/logs', {
-  //     repo: 'mwc1.tf16.toggl_task.code.dart',
-  //     owner: 'tpa-nextgen',
-  //     job_id: runJobs.data.jobs[0].id,
-  //   })
-  //   .catch(e => console.log(e))) as any;
+  //   const logs = (await octokit
+  //     .request('GET /repos/{owner}/{repo}/actions/jobs/{job_id}/logs', {
+  //       repo: 'mwc1.tf16.toggl_task.code.dart',
+  //       owner: 'tpa-nextgen',
+  //       job_id: runJobs.data.jobs[0].id,
+  //     })
+  //     .catch(e => console.log(e))) as any;
 
-  // let comment = '';
-  // let score;
-  // let comments;
+  //   let comment = '';
+  //   let score;
+  //   let comments;
 
-  // if (logs.data.includes('gotest')) {
-  //   if (logs.data.includes('Messages:')) {
-  //     comment = logs.data
-  //       .split('Messages:')[1]
-  //       .split('\r\n')[0]
-  //       .replace('\t', '')
-  //       .trim();
+  //   if (logs.data.includes('gotest')) {
+  //     if (logs.data.includes('Messages:')) {
+  //       comment = logs.data
+  //         .split('Messages:')[1]
+  //         .split('\r\n')[0]
+  //         .replace('\t', '')
+  //         .trim();
+  //     }
+  //     if (logs.data.includes('✓')) {
+  //       score = 3;
+  //       comment = 'Perfectly done.';
+  //     } else if (logs.data.includes('✖')) {
+  //       score = 1;
+  //     }
+  //   } else if (logs.data.includes('dart')) {
+  //     if (logs.data.includes('All tests passed')) {
+  //       score = 3;
+  //       comment = 'Perfectly done.';
+  //     } else if (logs.data.includes('tests failed')) {
+  //       score = 1;
+  //       comments = logs.data.split('[E]').map((el: any, i: number) => {
+  //         if (i === 0) {
+  //           return;
+  //         }
+  //         if (el.includes('UnimplementedError')) {
+  //           return;
+  //         }
+  //         const string = el.split('\r\n')[1];
+  //         const indexOfSpace = string.indexOf(' ');
+  //         return string.substring(indexOfSpace + 1).trim();
+  //       });
+  //       comment =
+  //         comments.length > 0 ? comments.filter((n: any) => n).join('.') : '';
+  //     }
   //   }
-  //   if (logs.data.includes('✓')) {
-  //     score = 3;
-  //     comment = 'Perfectly done.';
-  //   } else if (logs.data.includes('✖')) {
-  //     score = 1;
-  //   }
-  // } else if (logs.data.includes('dart')) {
-  //   if (logs.data.includes('All tests passed')) {
-  //     score = 3;
-  //     comment = 'Perfectly done.';
-  //   } else if (logs.data.includes('tests failed')) {
-  //     score = 1;
-  //     comments = logs.data.split('[E]').map((el: any, i: number) => {
-  //       if (i === 0) {
-  //         return;
-  //       }
-  //       if (el.includes('UnimplementedError')) {
-  //         return;
-  //       }
-  //       const string = el.split('\r\n')[1];
-  //       const indexOfSpace = string.indexOf(' ');
-  //       return string.substring(indexOfSpace + 1).trim();
-  //     });
-  //     comment =
-  //       comments.length > 0 ? comments.filter((n: any) => n).join('.') : '';
-  //   }
-  // }
 
   res.status(200).send({ taskDetails });
   // }
