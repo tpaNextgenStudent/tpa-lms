fe852629ee4a67c1f09d4b9f066b5986a5711cef
Patryk Górka <patryk.gorka@appunite.com>
2022-05-24 14:04:26 +0200

    tests: add TaskScoreBadge component story

---
+++ components/tasks/TaskScoreBadge/TaskScoreBadge.stories.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import { TaskScoreBadge } from './TaskScoreBadge';
+
+export default {
+  title: 'Tasks/TaskScoreBadge',
+  component: TaskScoreBadge,
+} as ComponentMeta<typeof TaskScoreBadge>;
+
+const Template: ComponentStory<typeof TaskScoreBadge> = args => (
+  <TaskScoreBadge {...args} />
+);
+
+export const Default = Template.bind({});
+Default.args = {
+  score: 3,
+};
+
+export const WithBorder = Template.bind({});
+WithBorder.args = {
+  score: 3,
+  withBorder: true,
+};
+
+export const WithText = Template.bind({});
+WithText.args = {
+  score: 3,
+  withText: true,
+};
+
+export const WithBorderAndText = Template.bind({});
+WithBorderAndText.args = {
+  score: 3,
+  withBorder: true,
+  withText: true,
+};
