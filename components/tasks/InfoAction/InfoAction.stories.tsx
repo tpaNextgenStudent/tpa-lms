3ab9d9425d483bf07661815aa95cac7d39483fe5
Patryk Górka <patryk.gorka@appunite.com>
2022-05-24 13:22:39 +0200

    tests: add InfoAction component story

---
+++ components/tasks/InfoAction/InfoAction.stories.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import { InfoAction } from './InfoAction';
+
+export default {
+  title: 'Common/InfoAction',
+  component: InfoAction,
+  parameters: { controls: { exclude: ['sizeRef'] } },
+} as ComponentMeta<typeof InfoAction>;
+
+const Template: ComponentStory<typeof InfoAction> = args => (
+  <InfoAction {...args} />
+);
+
+export const Default = Template.bind({});
+Default.args = {
+  task: {
+    id: 'taskId',
+    name: 'Task Name',
+    description: 'Task description',
+    link: 'https://github.com/path/to/repository/with/task',
+    type: 'code',
+  },
+};
