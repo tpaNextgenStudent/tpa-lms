451b019f09551693033f9617e1f886589b63b080
Patryk Górka <patryk.gorka@appunite.com>
2022-05-24 10:56:42 +0200

    tests: add HandleBold component story

---
+++ components/common/HandleBold/HandleBold.stories.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import { HandleBold } from './HandleBold';
+
+export default {
+  title: 'Common/HandleBold',
+  component: HandleBold,
+} as ComponentMeta<typeof HandleBold>;
+
+const Template: ComponentStory<typeof HandleBold> = args => (
+  <HandleBold {...args} />
+);
+
+export const Default = Template.bind({});
+Default.args = {
+  children: 'Text is *bolded* :D',
+};
